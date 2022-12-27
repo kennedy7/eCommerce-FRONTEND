81277f7efded319c2db0aa0ee0cc78d52d57e6c9
unknown <kennedyhillary6@gmail.com>
2022-12-27 02:16:51 +0100

    components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
+import Cart from './components/Cart';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        
+      <NavBar/>
+      <Routes>
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/" element={<Home/>} />
+      </Routes>
       
+      </Router>
     </div>
   );
 }
+
+
 
 export default App;
