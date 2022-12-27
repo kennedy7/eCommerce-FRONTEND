4df711c4b65108a244e2df60e8275a2d28cae41f
unknown <kennedyhillary6@gmail.com>
2022-12-27 18:43:14 +0100

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Cart from './components/Cart';
 import Home from './components/Home';
 import NavBar from './components/NavBar';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,10 +13,14 @@ function App() {
         
       <NavBar/>
       <Routes>
+
         <Route path="/cart" element={<Cart/>} />
-        <Route path="/" element={<Home/>} />
-      </Routes>
+        <Route path="/not-found" element={<NotFound/>} />
+        <Route path="/" exact element={<Home/>} />
+        <Route path="*" element={<Navigate to ="/not-found" />}/>
       
+
+      </Routes>
       </Router>
     </div>
   );
