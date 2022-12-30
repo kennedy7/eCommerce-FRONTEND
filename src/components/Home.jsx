5513b3ee7aeac91b77b79afd2b319e6063765081
unknown <kennedyhillary6@gmail.com>
2022-12-30 10:01:50 +0100

    rtk query

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
+import { useGetAllProductsQuery } from "../features/productsApi";
+
 const Home = () => {
+    const { data, error, isFetching, isLoading } = useGetAllProductsQuery()
     return ( <h2>Home</h2> );
 }
  
