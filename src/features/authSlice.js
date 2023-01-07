e335f84fa7ad6b32ad47a482d75dc798db5a5c81
unknown <kennedyhillary6@gmail.com>
2023-01-07 13:38:01 +0100

    authSlice

---
+++ src/features/authSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  name: "",
+  email: "",
+  _id: "",
+  registerStatus: "",
+  registerError: "",
+  loginStatus: "",
+  loginError: "",
+  userLoaded: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: {},
+});
