5325e8feeade801e2d1f28121502e7930d7dd85a
unknown <kennedyhillary6@gmail.com>
2023-01-09 04:44:56 +0100

    cart-login

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import productsReducer, { productsFetch } from "./features/productsSlice";
 import { productsApi } from "./features/productsApi";
 import cartReducer, { getTotals } from "./features/cartSlice";
-import authReducer, { loadUser, logOutUser } from "./features/authSlice";
+import authReducer, { loadUser } from "./features/authSlice";
 
 const store = configureStore({
   reducer: {
