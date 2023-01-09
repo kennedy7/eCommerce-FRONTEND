f3c35218c8377ea385a4ff5c45e02d02cbbbce63
unknown <kennedyhillary6@gmail.com>
2023-01-09 15:13:58 +0100

    stripe redirect

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -3,8 +3,18 @@ import { useSelector } from "react-redux";
 import { url } from "../slices/api";
 
 const PayButton = ({ cartItems }) => {
+  const user = useSelector((state) => state.auth);
   const handleCheckout = () => {
-    console.log(cartItems);
+    axios
+      .post(`${url}/stripe/create-checkout-session`, {
+        cartItems,
+        userId: user._id,
+      })
+      .then((res) => {
+        if (res.data.url) {
+          window.location = res.data.url;
+        }
+      });
   };
   return (
     <>
