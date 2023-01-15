41548306e6ef4585e43aaf66d0fc3433d49ad2ba
unknown <kennedyhillary6@gmail.com>
2023-01-15 09:46:48 +0100

    refactor

--- src/slices/productsApi.js
+++ src/slices/productsApi.js
@@ -1,12 +1,12 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { url } from "./api";
 export const productsApi = createApi({
-    reducerPath: "productsApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
-    endpoints: (build) => ({
-        getAllProducts: build.query({
-            query: () => "products",
-        })
-    })
-})
+  reducerPath: "productsApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${url}` }),
+  endpoints: (build) => ({
+    getAllProducts: build.query({
+      query: () => "products",
+    }),
+  }),
+});
 export const { useGetAllProductsQuery } = productsApi;
