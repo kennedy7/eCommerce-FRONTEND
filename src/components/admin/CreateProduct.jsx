1595cb59a58d7f28233f907201380bd7c4a1bdf0
unknown <kennedyhillary6@gmail.com>
2023-01-15 10:10:24 +0100

    createProduct style

--- src/components/admin/CreateProduct.jsx
+++ src/components/admin/CreateProduct.jsx
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+
 const CreateProduct = () => {
   return <h2>Create Product</h2>;
 };
 
 export default CreateProduct;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  margin-top: 2rem;
+  select,
+  input {
+    padding: 7px;
+    min-height: 30px;
+    outline: none;
+    border-radius: 5px;
+    border: 1px solid rgb(182, 182, 182);
+    margin: 0.3rem 0;
+    &:focus {
+      border: 2px solid rgb(0, 208, 255);
+    }
+  }
+  select {
+    color: rgb(95, 95, 95);
+  }
+`;
+
+const StyledCreateProduct = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ImagePreview = styled.div`
+  margin: 2rem 0 2rem 2rem;
+  padding: 2rem;
+  border: 1px solid rgb(183, 183, 183);
+  max-width: 300px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  color: rgb(78, 78, 78);
+  img {
+    max-width: 100%;
+  }
+`;
