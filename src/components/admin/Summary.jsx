edfe4bc02867f06e4ab3bbc7d80694b9d851b34f
unknown <kennedyhillary6@gmail.com>
2023-01-19 12:05:26 +0100

    summary react-icon

--- src/components/admin/Summary.jsx
+++ src/components/admin/Summary.jsx
@@ -1,7 +1,35 @@
 import styled from "styled-components";
 import { FaUsers, FaChartBar, FaClipBoard } from "react-icons";
 const Summary = () => {
-  const data = [{}];
+  const data = [
+    {
+      icon: <FaUsers />,
+      digits: 50,
+      isMoney: false,
+      title: "Users",
+      color: "rgb(102, 108, 255)",
+      bgColor: "rgba(102, 108, 255, 0.12)",
+      percentage: 30,
+    },
+    {
+      icon: <FaClipBoard />,
+      digits: 70,
+      isMoney: false,
+      title: "Others",
+      color: "rgb(38, 198, 249)",
+      bgColor: "rgb(38, 198, 249, 0.12)",
+      percentage: 30,
+    },
+    {
+      icon: <FaChartBar />,
+      digits: 500,
+      isMoney: false,
+      title: "Earnings",
+      color: "rgb(253, 181, 40)",
+      bgColor: "rgba(253, 181, 40, 0.12)",
+      percentage: 30,
+    },
+  ];
 
   return <h2>Summary</h2>;
 };
