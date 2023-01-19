4be8e868ed1db4656bbd6980267c0a74d2b84bf0
unknown <kennedyhillary6@gmail.com>
2023-01-19 11:55:16 +0100

    react-icon

--- src/components/admin/Summary.jsx
+++ src/components/admin/Summary.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import { FaUsers, FaChartBar, FaClipBoard } from "react-icons";
 const Summary = () => {
+  const data = [{}];
+
   return <h2>Summary</h2>;
 };
 
 export default Summary;
+
+const StyledSummary = styled.div`
+  width: 100%;
+  display: 100%;
+`;
+const MainStats = styled.div`
+  flex: 2;
+  width: 100%;
+`;
+const Title = styled.div`
+  p {
+    font-size: 14px;
+    color: rgba(234, 234, 255, 0.68);
+  }
+`;
