a1d8ce9459045dfc87fa782c0cf1531f34cd9269
unknown <kennedyhillary6@gmail.com>
2023-01-25 19:12:51 +0100

    orders summary

--- src/components/admin/Summary.jsx
+++ src/components/admin/Summary.jsx
@@ -14,13 +14,18 @@ const Summary = () => {
   console.log(ordersPerc);
 
   function compare(a, b) {
+    // if ((a._id || b._id) >= 12) {
+    //   return 1;
+    // }
     if (a._id < b._id) {
       return 1;
     }
     if (a._id > b._id) {
       return -1;
     }
-
+    // if ((a._id || b._id) >= 12) {
+    //   return 1;
+    // }
     return 0;
   }
 
