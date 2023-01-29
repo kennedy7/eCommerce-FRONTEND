ba782d45ac0a904b16df3e29a504cb51c665ad1d
unknown <kennedyhillary6@gmail.com>
2023-01-29 17:24:56 +0100

    admin summary Chart

--- src/components/admin/summary-components/Chart.jsx
+++ src/components/admin/summary-components/Chart.jsx
@@ -11,7 +11,47 @@ import {
 } from "recharts";
 
 const Chart = () => {
-  return <StyledChart> Chart </StyledChart>;
+  const data = [
+    {
+      day: "Mon",
+      amount: 4000,
+    },
+    {
+      day: "Tue",
+      amount: 3000,
+    },
+  ];
+
+  return (
+    <StyledChart>
+      <h3>Last 7 Days Earnings($)</h3>
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="day" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="amount"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </StyledChart>
+  );
 };
 
 export default Chart;
