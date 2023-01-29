921e8137504f7a116b410063ef1c2556f030dca8
unknown <kennedyhillary6@gmail.com>
2023-01-29 17:03:28 +0100

    admin summary Chart

---
+++ src/components/admin/summary-components/Chart.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+const Chart = () => {
+  return;
+};
+
+export default Chart;
+const StyledChart = styled.div`
+  width: 100%;
+  height: 300px;
+  margin-top: 2rem;
+  padding: 1rem;
+  border: 2px solid rgba(48, 51, 78, 0.2);
+  border-radius: 5px;
+`;
