d3843b7b2ce2b27b1865994122eb11f87a33becb
unknown <kennedyhillary6@gmail.com>
2023-02-06 19:19:16 +0100

    product view styling

--- src/components/Details/product.jsx
+++ src/components/Details/product.jsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+
 const Product = () => {
   return <>Product</>;
 };
 
 export default Product;
+
+const StyledProduct = styled.div`
+  margin: 3rem;
+  display: flex;
+  justify-content: center;
+`;
+const ProductContainer = styled.div`
+  max-width: 500px;
+  width: 100%;
+  height: auto;
+  display: flex;
+  box-shadow: rgba(100, 100, 111, 0.2);
+  border-radius: 5px;
+  padding: 2rem;
+`;
+const ImageContainer = styled.div`
+  flex: 1;
+  img {
+    width: 100%;
+  }
+`;
