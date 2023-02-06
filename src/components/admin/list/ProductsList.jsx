c7ea73227fbd298dc8117dd6833d365fb8c46f31
unknown <kennedyhillary6@gmail.com>
2023-02-06 16:41:49 +0100

    list style

--- src/components/admin/list/ProductsList.jsx
+++ src/components/admin/list/ProductsList.jsx
@@ -1,5 +1,27 @@
+import styled from "styled-components";
+
 const ProductsList = () => {
   return <>List</>;
 };
 
 export default ProductsList;
+
+const ImageContainer = styled.div`
+  img {
+    height: 40px;
+  }
+`;
+
+const Actions = styled.div`
+  width: 100px;
+  display: flex;
+  justify-content: space-between;
+  button {
+    border: none;
+    outline: none;
+    padding: 3px 5px;
+    color: white;
+    border-radius: 3px;
+    cursor: pointer;
+  }
+`;
