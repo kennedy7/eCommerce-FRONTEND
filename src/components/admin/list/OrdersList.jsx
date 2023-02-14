66c23ef6c13cff9d87e4d95dcb7321e2c32bdf3c
unknown <kennedyhillary6@gmail.com>
2023-02-14 10:40:22 +0100

    admin orders list

--- src/components/admin/list/OrdersList.jsx
+++ src/components/admin/list/OrdersList.jsx
@@ -1,5 +1,25 @@
+import styled from "styled-components";
+
 const OrdersList = () => {
   return <></>;
 };
 
 export default OrdersList;
+
+const DispatchBtn = styled.div`
+  backgound-color: rgba(38, 198, 249);
+`;
+const DeliveryBtn = styled.div`
+  backgound-color: rgba(102, 108, 255);
+`;
+
+const View = styled.button`
+  backgound-color: rgb(114, 225, 40);
+`;
+const Pending = styled.div`
+  color: rgb(253, 181, 40);
+  backgound-color: rgba(253, 181, 40, 0.12);
+  padding: 3px 5px;
+  border-radius: 3px;
+  font-size: 14px;
+`;
