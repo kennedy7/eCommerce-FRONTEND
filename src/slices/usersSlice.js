e8fb2bf58154bf8a4fd3065de035c51ab13197d4
unknown <kennedyhillary6@gmail.com>
2023-02-21 18:29:34 +0100

    usersSlice

---
+++ src/slices/usersSlice.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { setHeaders, url } from "./api";
+import { toast } from "react-toastify";
+
+const initialState = {
+  list: [],
+  status: null,
+  deleteStatus: null,
+};
+
+export const usersFetch = createAsyncThunk("users/usersFetch", async () => {
+  try {
+    const response = await axios.get(`${url}/users`, setHeaders());
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+export const userDelete = createAsyncThunk("users/userDelete", async (id) => {
+  try {
+    const response = await axios.delete(`${url}/users/${id}`, setHeaders());
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [usersFetch.pending]: (state, action) => {
+      state.status = "pending";
+    },
+  },
+});
