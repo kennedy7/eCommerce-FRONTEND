b90d20b4c8df1a7736ff7ca19123d93d6ff6aa28
unknown <kennedyhillary6@gmail.com>
2023-02-27 12:20:22 +0100

    refactor

--- src/components/admin/Users.jsx
+++ src/components/admin/Users.jsx
@@ -4,23 +4,9 @@ import UsersList from "./list/UsersList";
 const Users = () => {
   return (
     <div className="scrollable">
-      <Container></Container>
-
       <UsersList />
     </div>
   );
 };
 
 export default Users;
-
-const Container = styled.div`
-  display: flex;
-  padding: 15px 12px;
-  border-radius: 5px;
-  font-weight: 400;
-  letter-spacing: 1.15px;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  margin: 0.5rem 0;
-`;
