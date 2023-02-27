1718b39887d02fc7c6ee5e1070765ec98978d173
unknown <kennedyhillary6@gmail.com>
2023-02-27 11:37:44 +0100

    refactor

--- src/components/admin/Orders.jsx
+++ src/components/admin/Orders.jsx
@@ -1,11 +1,24 @@
 import OrdersList from "./list/OrdersList";
+import styled from "styled-components";
 
 const Orders = () => {
   return (
     <>
+      <Container> </Container>
       <OrdersList />
     </>
   );
 };
 
 export default Orders;
+const Container = styled.div`
+  display: flex;
+  padding: 15px 12px;
+  border-radius: 5px;
+  font-weight: 400;
+  letter-spacing: 1.15px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  margin: 0.5rem 0;
+`;
--- src/components/admin/Users.jsx
+++ src/components/admin/Users.jsx
@@ -1,11 +1,26 @@
+import styled from "styled-components";
 import UsersList from "./list/UsersList";
 
 const Users = () => {
   return (
     <>
+      <Container></Container>
+
       <UsersList />
     </>
   );
 };
 
 export default Users;
+
+const Container = styled.div`
+  display: flex;
+  padding: 15px 12px;
+  border-radius: 5px;
+  font-weight: 400;
+  letter-spacing: 1.15px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  margin: 0.5rem 0;
+`;
